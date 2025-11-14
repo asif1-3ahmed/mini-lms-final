685c4228f293d487854157d0bc977f947bf20c5a
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-11-14 08:51:37 +0530

    Finally

--- mini_lms_frontend/src/App.jsx
+++ mini_lms_frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import VerticalNavbar from "./components/VerticalNavbar";
 import CourseList from "./pages/CourseList";
 import CourseDetail from "./pages/CourseDetail";
@@ -9,19 +9,52 @@ import AdminDashboard from "./pages/AdminDashboard";
 import StudentDashboard from "./pages/StudentDashboard";
 import ProtectedRoute from "./components/PrivateRoute";
 
-export default function App(){
+export default function App() {
+  const location = useLocation();
+  const hideNavbar = ["/login", "/register"].includes(location.pathname);
+
   return (
-    <div className="min-h-screen flex">
-      <VerticalNavbar />
+    <div className="min-h-screen flex bg-gray-100">
+
+      {/* Sidebar only when logged in */}
+      {!hideNavbar && <VerticalNavbar />}
+
       <main className="flex-1 p-4">
         <Routes>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/" element={<Navigate to="/courses" />} />
-          <Route path="/courses" element={<CourseList/>} />
-          <Route path="/courses/:id" element={<CourseDetail/>} />
-          <Route path="/admin" element={<ProtectedRoute role="ADMIN"><AdminDashboard/></ProtectedRoute>} />
-          <Route path="/student" element={<ProtectedRoute role="STUDENT"><StudentDashboard/></ProtectedRoute>} />
+
+          {/* Auth pages */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Homepage → Login */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          {/* Courses (public browse allowed) */}
+          <Route path="/courses" element={<CourseList />} />
+          <Route path="/courses/:id" element={<CourseDetail />} />
+
+          {/* Dashboards (protected) */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute role="ADMIN">
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/student"
+            element={
+              <ProtectedRoute role="STUDENT">
+                <StudentDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Catch-all → Login */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+
         </Routes>
       </main>
     </div>
